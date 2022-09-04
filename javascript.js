cedc6d29a12251a7445b8dd3ebdf4d983825eac6
Amir <93699943+AmirSenpai@users.noreply.github.com>
2022-09-05 00:06:31 +0430

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function heart(){
+    let hearticon = document.getElementById("heart");
+    hearticon.className = "fas fa-heart i";
+    document.getElementById("likes").innerHTML = "2,000 likes";
+}
+function bookmark(){
+    let bookmark = document.getElementById("mark");
+    bookmark.className = "fas fa-bookmark i o";
+}
